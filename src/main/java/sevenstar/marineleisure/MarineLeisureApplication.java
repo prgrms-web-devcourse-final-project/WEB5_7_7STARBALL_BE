49b41e201d9ab44cbc7bdea27b76b7d91192f2ee
HwuanPage <gigolopage@gmail.com>
2025-06-30 17:47:02 +0900

    git initialize

---
+++ src/main/java/sevenstar/marineleisure/MarineLeisureApplication.java
@@ -0,0 +1,13 @@
+package sevenstar.marineleisure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarineLeisureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarineLeisureApplication.class, args);
+	}
+
+}
