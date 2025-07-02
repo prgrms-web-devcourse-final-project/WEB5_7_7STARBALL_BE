064cb240e33aa78eca61ff6dc09b6d569765ee64
gunwoong <gunwoong1630@gmail.com>
2025-07-02 15:43:47 +0900

    feat: swagger 추가

---
+++ src/main/java/sevenstar/marineleisure/global/swagger/SwaggerTestRequest.java
@@ -0,0 +1,18 @@
+package sevenstar.marineleisure.global.swagger;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+public class SwaggerTestRequest {
+	@Schema(description = "사용자 이름", example = "testuser")
+	private final String username;
+	@Schema(description = "이메일 주소", example = "test@gmail.com")
+	private final String email;
+	@Schema(description = "비밀번호", example = "1234")
+	private final String password;
+	@Schema(description = "이미지 파일", type = "string", format = "binary")
+	private final MultipartFile file;
+}
---
+++ src/main/java/sevenstar/marineleisure/global/swagger/SwaggerTestResponse.java
@@ -0,0 +1,9 @@
+package sevenstar.marineleisure.global.swagger;
+
+import lombok.Data;
+
+@Data
+public class SwaggerTestResponse {
+	private final String username;
+	private final String password;
+}
