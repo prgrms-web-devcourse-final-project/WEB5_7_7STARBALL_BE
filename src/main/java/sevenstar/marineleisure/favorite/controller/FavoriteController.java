6bd8f3adad7452e2778b52155eaa431db3049509
Hwang Seong Cheol a.k.a Hwuan Page <gigolopage@gmail.com>
2025-07-07 15:37:32 +0900

    Update FavoriteController.java

--- src/main/java/sevenstar/marineleisure/favorite/controller/FavoriteController.java
+++ src/main/java/sevenstar/marineleisure/favorite/controller/FavoriteController.java
@@ -5,13 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
-/**
- * MarineLeisure - FavoriteController
- * create date:    25. 7. 7.
- * last update:    25. 7. 7.
- * author:  gigol
- * purpose: favorite REST Controller
- */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/favorite")
