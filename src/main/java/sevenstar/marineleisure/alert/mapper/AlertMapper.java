12c3ed8d95dd7d222c82e5c3f023112acd5c68ae
Hwang Seong Cheol a.k.a Hwuan Page <gigolopage@gmail.com>
2025-07-07 15:36:19 +0900

    Update AlertMapper.java

--- src/main/java/sevenstar/marineleisure/alert/mapper/AlertMapper.java
+++ src/main/java/sevenstar/marineleisure/alert/mapper/AlertMapper.java
@@ -6,13 +6,6 @@ import lombok.RequiredArgsConstructor;
 import sevenstar.marineleisure.alert.domain.JellyfishRegionDensity;
 import sevenstar.marineleisure.alert.dto.response.JellyfishResponseDto;
 
-/**
- * MarineLeisure - AlertMapper
- * create date:    25. 7. 7.
- * last update:    25. 7. 7.
- * author:  gigol
- * purpose: 
- */
 @Component
 @RequiredArgsConstructor
 public class AlertMapper {
