8a23084e9ef85b755246733968e642c3ca288e55
Hwang Seong Cheol a.k.a Hwuan Page <gigolopage@gmail.com>
2025-07-07 15:36:35 +0900

    Update JellyfishRegionDensityRepository.java

--- src/main/java/sevenstar/marineleisure/alert/repository/JellyfishRegionDensityRepository.java
+++ src/main/java/sevenstar/marineleisure/alert/repository/JellyfishRegionDensityRepository.java
@@ -4,12 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import sevenstar.marineleisure.alert.domain.JellyfishRegionDensity;
 
-/**
- * MarineLeisure - JellyfishRepository
- * create date:    25. 7. 7.
- * last update:    25. 7. 7.
- * author:  gigol
- * purpose: 
- */
 public interface JellyfishRegionDensityRepository extends JpaRepository<JellyfishRegionDensity, Long> {
 }
