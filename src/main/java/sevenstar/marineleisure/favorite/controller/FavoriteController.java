8fdab29fd7c06ae31768155d5bda9b5bfe5b1e0d
Hwang Seong Cheol a.k.a Hwuan Page <gigolopage@gmail.com>
2025-07-07 15:49:40 +0900

    Feature/FavoritesAndAlertInterface-16-HwuanPage
    * feature/FavoritesAndAlertInterface-16-HwuanPage
    
    * Update AlertMapper.java
    
    * Update JellyfishRegionDensityRepository.java
    
    * Update AlertController.java
    
    * Update FavoriteController.java
    
    * Update FavoriteRepository.java
    
    * Update AlertController.java
    
    * Update JellyfishSpieces.java
    
    * Update JellyfishRegion.java
    
    * Update JellyfishRegion.java

---
+++ src/main/java/sevenstar/marineleisure/favorite/controller/FavoriteController.java
@@ -0,0 +1,13 @@
+package sevenstar.marineleisure.favorite.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/favorite")
+public class FavoriteController {
+
+}
