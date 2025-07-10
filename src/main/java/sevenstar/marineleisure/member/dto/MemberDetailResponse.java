35836d5dde2877a7ccc0b910587c88ab53ef5a97
JaeoneHeo <jaewonheo666@gmail.com>
2025-07-10 10:32:11 +0900

    feat: member 정보 조회하는 서비스 로직 구현

---
+++ src/main/java/sevenstar/marineleisure/member/dto/MemberDetailResponse.java
@@ -0,0 +1,21 @@
+package sevenstar.marineleisure.member.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import sevenstar.marineleisure.global.enums.MemberStatus;
+
+import java.math.BigDecimal;
+
+/**
+ * 회원 상세 정보 응답 DTO
+ */
+@Getter
+@Builder
+public class MemberDetailResponse {
+    private Long id;
+    private String email;
+    private String nickname;
+    private MemberStatus status;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+}
