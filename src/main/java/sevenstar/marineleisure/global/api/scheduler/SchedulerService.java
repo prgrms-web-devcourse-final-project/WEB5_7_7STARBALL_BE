24b1b00232ccb63c7efc23ee5fe8b328ff9a69af
gunwoong <gunwoong1630@gmail.com>
2025-07-10 13:56:32 +0900

    feat: 조회도 관련 서비스 추가

--- src/main/java/sevenstar/marineleisure/global/api/scheduler/SchedulerService.java
+++ src/main/java/sevenstar/marineleisure/global/api/scheduler/SchedulerService.java
@@ -5,9 +5,11 @@ import java.time.LocalDate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import sevenstar.marineleisure.global.api.khoa.service.KhoaApiService;
 import sevenstar.marineleisure.global.api.openmeteo.dto.service.OpenMeteoService;
+import sevenstar.marineleisure.spot.repository.SpotViewQuartileRepository;
 
 @Service
 @RequiredArgsConstructor
@@ -15,16 +17,18 @@ public class SchedulerService {
 	public static final int MAX_EXPECT_DAY = 3;
 	private final KhoaApiService khoaApiService;
 	private final OpenMeteoService openMeteoService;
-
+	private final SpotViewQuartileRepository spotViewQuartileRepository;
 	/**
 	 * 앞으로의 스케줄링 전략에 의해 수정될 부분입니다.
 	 * @author guwnoong
 	 */
-	@Scheduled(initialDelay = 0, fixedDelay = 259200000)
+	@Scheduled(initialDelay = 0, fixedDelay = 86400000)
+	@Transactional
 	public void scheduler() {
 		LocalDate today = LocalDate.now();
 
 		khoaApiService.updateApi(today, today.plusDays(MAX_EXPECT_DAY));
 		openMeteoService.updateApi(today, today.plusDays(MAX_EXPECT_DAY));
+		spotViewQuartileRepository.upsertQuartile();
 	}
 }
