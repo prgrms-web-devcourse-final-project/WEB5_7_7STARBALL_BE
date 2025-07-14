816a88df62825e6d5b7300943e109ca0bc448ffb
LEESUNBIN <45359953+garusitell@users.noreply.github.com>
2025-07-14 16:23:23 +0900

    Delete src/main/java/sevenstar/marineleisure/meeting/repository/OutdoorSpotSpotRepository.java

--- src/main/java/sevenstar/marineleisure/meeting/repository/OutdoorSpotSpotRepository.java
+++
@@ -1,9 +0,0 @@
-package sevenstar.marineleisure.meeting.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import sevenstar.marineleisure.spot.domain.OutdoorSpot;
-
-public interface OutdoorSpotSpotRepository extends JpaRepository<OutdoorSpot,Long> {
-
-}
