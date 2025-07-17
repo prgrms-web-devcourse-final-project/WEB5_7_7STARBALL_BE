35a8260de230b7feb37723c5cbee8c3d6522def2
gunwoong <gunwoong1630@gmail.com>
2025-07-17 11:27:00 +0900

    refactoring: GeoUtils refactoring

--- src/main/java/sevenstar/marineleisure/global/utils/GeoUtils.java
+++ src/main/java/sevenstar/marineleisure/global/utils/GeoUtils.java
@@ -6,12 +6,13 @@ import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import sevenstar.marineleisure.global.api.kakao.KakaoApiClient;
 import sevenstar.marineleisure.global.enums.Region;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class GeoUtils {
@@ -19,21 +20,12 @@ public class GeoUtils {
 	private final KakaoApiClient kakaoApiClient;
 
 	public Point createPoint(BigDecimal latitude, BigDecimal longitude) {
-		return geometryFactory.createPoint(new Coordinate(latitude.doubleValue(),longitude.doubleValue()));
+		return geometryFactory.createPoint(new Coordinate(longitude.doubleValue(), latitude.doubleValue()));
 	}
 
 	public Region searchRegion(float latitude, float longitude) {
 		return Region.fromAddress(kakaoApiClient.get(latitude, longitude).getBody().getDocuments().getFirst()
 			.getAddress_name());
 	}
-
-	public static double meterToBufferDegree(double meter) {
-		return meter / 111_000.0;
-	}
-
-	public static double kmToMeter(int km) {
-		return km * 1000;
-	}
-
 
 }
