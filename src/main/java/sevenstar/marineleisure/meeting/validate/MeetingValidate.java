2b203f228630e7e193a03e6d17d5bdb6a97c37d6
iseonbin <twinkey0201@gmail.com>
2025-07-23 20:39:57 +0900

    refactor: RichDomain으로 변경 내역입니다.

--- src/main/java/sevenstar/marineleisure/meeting/validate/MeetingValidate.java
+++ src/main/java/sevenstar/marineleisure/meeting/validate/MeetingValidate.java
@@ -25,39 +25,55 @@ public class MeetingValidate {
 
 	}
 
+	// Rich Domain Model 리팩토링으로 불필요해진 메서드들
+	// Meeting.isHost()로 대체됨
+	/*
 	@Transactional(readOnly = true)
 	public void verifyIsHost(Long memberId, Long hostId){
 		if(!Objects.equals(hostId, memberId)){
 			throw new CustomException(MeetingError.MEETING_NOT_HOST);
 		}
 	}
+	*/
 
+	// Meeting.canJoin()로 대체됨
+	/*
 	@Transactional(readOnly = true)
 	public void verifyRecruiting(Meeting meeting){
 		if(meeting.getStatus() != MeetingStatus.RECRUITING){
 			throw new CustomException(MeetingError.MEETING_NOT_RECRUITING);
 		}
 	}
+	*/
 
+	// Meeting.isFull()로 대체됨
+	/*
 	@Transactional(readOnly = true)
 	public void verifyMeetingCount(int targetCount, Meeting meeting){
 		if(targetCount >= meeting.getCapacity()){
 			throw new CustomException(MeetingError.MEETING_ALREADY_FULL);
 		}
 	}
+	*/
 
+	// Meeting.removeParticipant()에서 처리됨
+	/*
 	@Transactional(readOnly = true)
 	public void verifyNotHost(Long memberId, Meeting meeting){
 		if(memberId.equals(meeting.getHostId())){
 			throw new CustomException(MeetingError.MEETING_NOT_LEAVE_HOST);
 		}
 	}
+	*/
 
+	// Meeting.canLeave()로 대체됨
+	/*
 	@Transactional(readOnly = true)
 	public void verifyLeave(Meeting meeting){
 		if(meeting.getStatus() == MeetingStatus.COMPLETED || meeting.getStatus() == MeetingStatus.ONGOING){
 			throw new CustomException(MeetingError.CANNOT_LEAVE_COMPLETED_MEETING);
 		}
 	}
+	*/
 
 }
