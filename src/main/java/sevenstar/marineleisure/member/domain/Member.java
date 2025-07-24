c6ba296f80769edbcdce0fae50f6852fa65ea3f7
JaeoneHeo <jaewonheo666@gmail.com>
2025-07-24 22:38:17 +0900

    fix: member entity의 nickname 중복을 허용한다

--- src/main/java/sevenstar/marineleisure/member/domain/Member.java
+++ src/main/java/sevenstar/marineleisure/member/domain/Member.java
@@ -25,7 +25,7 @@ public class Member extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(nullable = false, length = 20, unique = true)
+	@Column(nullable = false, length = 20)
 	private String nickname;
 
 	@Column(nullable = false, length = 50, unique = true)
