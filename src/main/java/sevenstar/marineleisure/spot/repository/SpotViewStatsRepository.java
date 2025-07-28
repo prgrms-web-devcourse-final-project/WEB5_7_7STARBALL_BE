d1bb05d5b75066cd5e295d3cb2c62f5bbde98e94
gunwoong <gunwoong1630@gmail.com>
2025-07-28 13:46:25 +0900

    fix: 사용하지않는 projeciton 삭제

--- src/main/java/sevenstar/marineleisure/spot/repository/SpotViewStatsRepository.java
+++ src/main/java/sevenstar/marineleisure/spot/repository/SpotViewStatsRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.repository.query.Param;
 
 import sevenstar.marineleisure.spot.domain.SpotViewStats;
 import sevenstar.marineleisure.spot.domain.SpotViewStatsId;
-import sevenstar.marineleisure.spot.dto.projection.SpotViewStatsProjection;
 
 public interface SpotViewStatsRepository extends JpaRepository<SpotViewStats, SpotViewStatsId> {
 
@@ -19,16 +18,4 @@ public interface SpotViewStatsRepository extends JpaRepository<SpotViewStats, Sp
 				   VALUES (:spotId,:viewDate,1) ON DUPLICATE KEY UPDATE view_count = view_count + 1
 		""", nativeQuery = true)
 	void upsertViewStats(@Param("spotId") Long spotId, @Param("viewDate") LocalDate viewDate);
-
-	@Query(value = """
-		SELECT
-		    SUM(CASE WHEN view_date BETWEEN :targetDate - INTERVAL 6 DAY AND :targetDate THEN view_count ELSE 0 END) AS weekly_count,
-		    SUM(CASE WHEN view_date BETWEEN :targetDate - INTERVAL 29 DAY AND :targetDate THEN view_count ELSE 0 END) AS monthly_count
-		FROM spot_view_stats
-		WHERE spot_id = :spotId
-		  AND view_date BETWEEN :targetDate - INTERVAL 29 DAY AND :targetDate
-		""", nativeQuery = true)
-	SpotViewStatsProjection findSpotViewStatsBySpotId(@Param("spotId") Long spotId,
-		@Param("targetDate") LocalDate targetDate);
-
 }
