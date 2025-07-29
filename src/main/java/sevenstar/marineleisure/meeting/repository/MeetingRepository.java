ccc9ac6dc04cf3c543ad1c0d5d3bc4cf0077bbbe
JaeoneHeo <jaewonheo666@gmail.com>
2025-07-29 17:32:47 +0900

    hotfix: 탈퇴 시 딜리트 modify로 수정.

--- src/main/java/sevenstar/marineleisure/meeting/repository/MeetingRepository.java
+++ src/main/java/sevenstar/marineleisure/meeting/repository/MeetingRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -54,4 +55,8 @@ public interface MeetingRepository extends JpaRepository<Meeting, Long> {
 	@Query("SELECT m FROM Meeting m WHERE m.id = :meetingId")
 	Optional<Meeting> findByIdWithLock(@Param("meetingId") Long meetingId);
 
+
+	@Modifying
+	@Query("DELETE FROM Meeting m WHERE m.hostId = :hostId")
+	int deleteMeetingByHostId(@Param("hostId") Long hostId);
 }
--- src/main/java/sevenstar/marineleisure/meeting/repository/ParticipantRepository.java
+++ src/main/java/sevenstar/marineleisure/meeting/repository/ParticipantRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -29,4 +30,8 @@ public interface ParticipantRepository extends JpaRepository<Participant, Long> 
 
 	@Query("SELECT p.meetingId, COUNT(p) FROM Participant p WHERE p.meetingId IN :meetingIds GROUP BY p.meetingId")
 	List<Object[]> countByMeetingIdIn(@Param("meetingIds") List<Long> meetingIds);
+
+	@Modifying
+	@Query("DELETE FROM Participant p WHERE p.userId = :userId")
+	int deleteByUserId(@Param("userId") Long userId);
 }
